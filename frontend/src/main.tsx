a7d0bd3eade59068ae3238f61b5278334c705fda
Jervin <andoyjervin1@gmail.com>
2026-05-05 11:54:13 +0800

    feat: initialize Amplify and wrap app with AuthProvider

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,14 @@
+import './lib/amplify'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
+import { AuthProvider } from './context/AuthContext'
 import './styles/globals.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 )

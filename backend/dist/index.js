76aa78ab01c217cb4b52fa6c861f4b747149af53
Jervin <andoyjervin1@gmail.com>
2026-05-22 22:36:01 +0800

    fix: admin page | user role function (unable to change)

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -23,7 +23,7 @@ app.use(cors({
         return callback(new Error("CORS blocked"));
     },
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Ngrok-Skip-Browser-Warning'],
 }));
 app.use(express.json({ limit: "10mb" }));
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -29,7 +29,7 @@ app.use(
 			return callback(new Error("CORS blocked"));
 		},
 		credentials: true,
-		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+		methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
 		allowedHeaders: ['Content-Type', 'Authorization', 'Ngrok-Skip-Browser-Warning'],
 	}),
 );
